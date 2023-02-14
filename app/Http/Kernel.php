8c9269cdcc7b0fbc7b9b5c112635e854b4cdb1ae
tcampbPPU <tcamp022@gmail.com>
2023-02-14 12:35:35 -0500

    put back until 10 works

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $middlewareAliases = [
+    protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
